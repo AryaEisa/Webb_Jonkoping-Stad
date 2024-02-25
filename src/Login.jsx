43a083c4cb943ff33d51dd4ee15a487f994c6147
arya_irse@yahoo.com <arya_irse@yahoo.com>
2024-02-25 16:59:42 +0100

    Created the body for Login.

---
+++ src/Login.jsx
@@ -0,0 +1,31 @@
+
+import React, { useState } from 'react';
+import './Login.css'; //
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Login details:', username, password);
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label className="form-label">Username:</label>
+          <input className="form-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Password:</label>
+          <input className="form-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button className="form-submit" type="submit">Logga in</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
